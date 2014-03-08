beef2dd1edd1d855e3eca3cd4b1091e92dcbcab3
Adam Wentz <awentz@theonion.com>
2014-03-08 17:26:41 -0600

    Added dev http server vcK

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,24 +2,38 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      build: {
-        src: '<%= pkg.main %>',
-        dest: '<%= pkg.name %>.min.js'
+      pkg: grunt.file.readJSON('package.json'),
+      uglify: {
+          build: {
+              src: '<%= pkg.main %>',
+              dest: '<%= pkg.name %>.min.js'
+          }
+      },
+      jscrush: {
+          build: {
+              src: '<%= pkg.name %>.min.js',
+              dest: '<%= pkg.name %>.crushed.js'
+          }
+      },
+      'http-server': {
+          'serve': {
+              root: '.',
+              port: 8000,
+              host: '127.0.0.1',
+              cache: 0,
+              showDir : true,
+              autoIndex: true,
+              defaultExt: "html",
+              runInBackground: false
+          }
       }
-    },
-    jscrush: {
-      build: {
-        src: '<%= pkg.name %>.min.js',
-        dest: '<%= pkg.name %>.crushed.js'
-      }
-    }
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-http-server');
   grunt.loadNpmTasks('grunt-jscrush');
-  // Default task(s).
+  
   grunt.registerTask('default', ['uglify', 'jscrush']);
+  grunt.registerTask('serve', ['http-server']);
 
 };
--- README.md
+++ README.md
@@ -7,7 +7,8 @@ Usage
  * Install dependencies with `npm install`
  * Create interesting javascript in js1k.js
  * Run `grunt` to make an optimized build in `js1k.crushed.js`
- * Point your browser at `index.html` or `index-crushed.html` to view in the contest shim
+ * Run `grunt serve` to start a local webserver at http://localhost:8000/
+ * Point your browser at [index.html](http://localhost:8000/index.html) or [index-crushed.html](http://localhost:8000/index.html) to view in the contest shim
  * Submit to [js1k](http://js1k.com/)
 
 License
